bf755177006efa52c00642f7cc536087cc2d4b35
minkyeng <minnn.xyz@gmail.com>
2024-03-19 16:10:44 +0900

    feat : day2 challenge3 완료
    프로그래머스 '문자열 나누기'
    * solution.py : 코드 작성
    * solution.md : 문제 풀이

--- python-codingtest/day2/challenge3/solution.py
+++ python-codingtest/day2/challenge3/solution.py
@@ -1,40 +1,20 @@
 # 문자열 나누기
 
 def solution(s):
-    answer = 0
-
-    x = a = ''
-    x_num = etc_num = 0
-
-    a_list = []
+    result = x_num = etc_num = 0
+    x = ''
 
     for i in range(0, len(s)):
-
-        a += s[i]
-
         if x == '':
             x = s[i]
-            x_num = 1
-            continue
 
         if x == s[i]:
             x_num += 1
         else:
             etc_num += 1
 
-        if x_num == etc_num:
-            a_list.append(a)
-            x = a = ''
-            etc_num = 0
+        if (x_num == etc_num) | (i == len(s)-1):
+            result += 1
+            x = ''
 
-        if i == len(s)-1:
-            a_list.append(a)
-
-    answer = len(a_list)
-
-    return answer
-
-
-print(solution('banana'))
-print(solution('abracadabra'))
-print(solution('aaabbaccccabba'))
+    return result

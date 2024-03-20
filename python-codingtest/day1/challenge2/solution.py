140e90ca6aa8f3ee17dd360d9d668af505bf1283
minkyeng <minnn.xyz@gmail.com>
2024-03-20 14:11:54 +0900

    fix : day1 challenge2 코드 수정
    프로그래머스 '369게임'
    * solution.py : 코드 작성
    * solution.md : 문제 풀이

--- python-codingtest/day1/challenge2/solution.py
+++ python-codingtest/day1/challenge2/solution.py
@@ -2,13 +2,9 @@
 
 def solution(order):
     result = 0
-    checks = [3, 6, 9]
-    orders = list(map(int, str(order)))
+    orders = list(str(order))
 
-    for check in checks:
-        for order in orders:
-            if order == check:
-                result += 1
-                orders.pop(orders.index(order))
-            continue
+    for i in [3, 6, 9]:
+        result += orders.count(str(i))
+
     return result
